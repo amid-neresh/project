8b16d6b3684e3ccf574d25c47810c6ce2e433e4b
Dima <dima.sheren@gmail.com>
2023-12-05 13:11:06 +0200

    movie object

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 'use strict';
 
-const userName = 'Dima';
-let age = 33;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+// console.log(personalMovieDB);
+
+let lastWatch1 = prompt('Один из последних просмотреных фильмов?', '');
+let movieReiting1 = +prompt('На сколько оцените его?', '');
+let lastWatch2 = prompt('Один из последних просмотреных фильмов?', '');
+let movieReiting2 = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastWatch1] = movieReiting1;
+personalMovieDB.movies[lastWatch2] = movieReiting2;
+
+console.log(personalMovieDB);
